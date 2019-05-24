85dd9a1fea7c2bc38f31b1c529a92e1bb643dce1
Olufemi Oye <olufemi.oye@gmail.com>
2019-05-24 06:14:13 +0100

    resume animations

--- src/components/ResumeEnvelope.vue
+++ src/components/ResumeEnvelope.vue
@@ -3,7 +3,7 @@
 		<div class="resume-envelope__back"></div>
 		<div class="resume-paper"></div>
 		<div class="resume-envelope__front">
-			<span class="resume-envelope__cta">Click to view resume</span>
+			<span class="resume-envelope__cta">Olufemi's Resume</span>
 		</div>
 	</a>
 </template>
@@ -40,9 +40,17 @@ $magnifiaction: 4/3;
 	height: $envelope-height;
 	width: $envelope-width;
 	margin: 0 auto;
+	transition: .5s ease-in-out all;
 
 	@include breakpoint-min(xl) {
 		transform: scale(1.33333333333333)
+	}
+
+	&:hover,&:focus {
+		.resume-paper {
+			top: 0rem;
+			animation: none;
+		}
 	}
 }
 
@@ -52,7 +60,7 @@ $magnifiaction: 4/3;
 	left: 0;
 	width: $envelope-width;
 	height: $back-height;
-	background: $back-color;
+	background: darken($back-color, 15%);
 
 	&::before {
 		content: '';
@@ -64,13 +72,16 @@ $magnifiaction: 4/3;
 		border-style: solid;
 		border-width: 0 ($envelope-width/2) $back-flap-height ($envelope-width/2);
 		border-color: transparent transparent $back-color transparent;
+		transition: .5s ease-in-out all;
+		transition-delay: .5s;
+		transform-origin: bottom;
 	}
 }
 
 .resume-envelope__front {
 	display: flex;
 	justify-content: center;
-	align-items: center;
+	align-items: flex-end;
 	position: absolute;
 	bottom: 0;
 	left: 0;
@@ -103,8 +114,9 @@ $magnifiaction: 4/3;
 	color: $back-color;
 	font-size: .875rem;
 	text-align: center;
-	padding: .5rem;
-	border: 1px solid $back-color;
+	margin-bottom: 2rem;
+	// padding: .5rem;
+	// border: 1px solid $back-color;
 }
 
 .resume-paper {
@@ -116,16 +128,53 @@ $magnifiaction: 4/3;
 	background: $white;
 	border: 1px solid rgba($snow, .5);
 	box-shadow: 0 0 .25rem rgba($black, .08);
+	transition: inherit;
+	animation: 10s bounce infinite;
+	animation-delay: 3s;
 	&::after {
-		content: "Olufemi's Resume";
+		content: "Click to download ";
 		display: block;
 		position: relative;
 		font-size: .75rem;
 		color: $smoke;
 		text-align: center;
 		line-height: 1.5; 
-		margin-top: 1.5rem;
+		margin-top: 3rem;
 	}
+}
+
+@keyframes bounce {
+  from,
+  3%,
+  7%,
+  10%,
+  to {
+    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    transform: translate3d(0, 0, 0);
+  }
+
+  5%,
+  6% {
+    animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+    transform: translate3d(0, -1rem, 0);
+  }
+
+  8% {
+    animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+    transform: translate3d(0, -.5rem, 0);
+  }
+
+  11% {
+    transform: translate3d(0, -.125rem, 0);
+	}
+	13% {
+		transform: translate3d(0, 0, 0);
+	}
+}
+
+.bounce {
+  animation-name: bounce;
+  transform-origin: center bottom;
 }
 
 </style>
